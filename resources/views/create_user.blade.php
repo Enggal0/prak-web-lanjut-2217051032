2e4191cad658075b3ee9a299a5730d495b2f6c03
Enggal0 <enggalbimas8@gmail.com>
2024-10-04 22:39:56 +0700

    read data

--- resources/views/create_user.blade.php
+++ resources/views/create_user.blade.php
@@ -1,97 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Student Information Form</title>
-  <style>
-    body {
-      background-image: url('https://wallpapercave.com/wp/wp2468660.jpg');
-      background-size: cover; 
-      background-position: center; 
-      background-repeat: no-repeat;
-      font-family: 'Montserrat', 'Poppins', sans-serif;
-      /* background-color: #000072; */
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      height: 100vh;
-      margin: 0;
-      color: white;
-    }
-
-    .form-container {
-      /* background-color: #1db954;  */
-      background-color: #000072;
-      padding: 30px;
-      border-radius: 10px;
-      box-shadow: 0 4px 15px rgba(0, 0, 0, 0.3);
-      width: 350px;
-    }
-
-    h1 {
-      text-align: center;
-      /* color: #1db954; */
-      color:white;
-      margin-bottom: 25px;
-      font-size: 24px;
-    }
-
-    label {
-      display: block;
-      margin-bottom: 14px;
-      font-weight: bold;
-      color: #fff;
-      font-size: 14px;
-    }
-
-    input[type="text"] {
-      font-family: 'Montserrat', 'Poppins', sans-serif;
-      width: 91%;
-      padding: 10px 15px;
-      margin-bottom: 20px;
-      border: none;
-      border-radius: 4px;
-      font-size: 16px;
-      background-color: #fff;
-      color: #333;
-    }
-
-    input[type="submit"] {
-      font-family: 'Montserrat', 'Poppins', sans-serif;
-      width: 100%;
-      padding: 12px;
-      background-color: #1db954;
-      color: white;
-      border: none;
-      border-radius: 4px;
-      cursor: pointer;
-      font-size: 18px;
-      transition: background-color 0.3s;
-    }
-
-    select{
-      font-family: 'Montserrat', 'Poppins', sans-serif;
-      margin-bottom:15px;
-      border-radius: 4px;
-    }
-
-    input[type="submit"]:hover {
-      font-family: 'Montserrat', 'Poppins', sans-serif;
-      background-color: #0000a3;
-      font-style: bold;
-    }
-
-    .error-msg {
-      color: red;
-      font-size: 12px;
-      margin-top: -15px;
-      margin-bottom: 10px;
-    }
-
-  </style>
-</head>
-<body>
+@extends('layouts.app') 
+ 
+@section('content') 
+<div> 
   <div class="form-container">
     <h1>Formulir Biodata Mahasiswa</h1>
     <form action="{{ route('user.store') }}" method="POST">
@@ -120,5 +30,5 @@
       <input type="submit" value="Submit">
     </form>
   </div>
-</body>
-</html>
+</div> 
+  @endsection
---
+++ resources/views/list_user.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app') 
+
+@section('content') 
+<div class="container">
+    <table> 
+       <thead> 
+          <tr> 
+             <th>ID</th> 
+             <th>Nama</th> 
+             <th>NPM</th> 
+             <th>Kelas</th> 
+             <th>Aksi</th> 
+          </tr> 
+       </thead> 
+       <tbody> 
+          <?php 
+          foreach ($users as $user) { 
+          ?> 
+             <tr> 
+                <td><?= $user['id'] ?></td> 
+                <td><?= $user['nama'] ?></td> 
+                <td><?= $user['npm'] ?></td> 
+                <td><?= $user['nama_kelas'] ?></td> 
+                <td></td> 
+             </tr> 
+          <?php 
+          } 
+          ?> 
+       </tbody> 
+    </table> 
+</div>
+@endsection
