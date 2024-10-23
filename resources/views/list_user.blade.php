a6b70d787f248d8e7cd619d72d328ea29d89b461
Enggal0 <enggalbimas8@gmail.com>
2024-10-23 07:56:01 +0700

    update delete

--- resources/views/list_user.blade.php
+++ resources/views/list_user.blade.php
@@ -1,32 +1,48 @@
-@extends('layouts.app') 
+@extends('layouts.app')
 
-@section('content') 
-<div class="container">
-<a href="{{ route('user.create') }}" class="btn-tambah">Tambah Pengguna Baru</a>
+@section('content')
+<div class="container my-4">
+    <a href="{{ route('user.create') }}" class="btn btn-primary mb-3">Tambah Pengguna Baru</a>
 
-   <table> 
-       <thead> 
-          <tr> 
-             <th>ID</th> 
-             <th>Nama</th> 
-             <th>NPM</th> 
-             <th>Kelas</th> 
-             <th>Aksi</th> 
-          </tr> 
-       </thead> 
-       <tbody> 
-       @foreach ($users as $user)
+    <div class="table-responsive">
+        <table class="table table-striped table-bordered">
+            <thead class="table-dark">
                 <tr>
-                  <td><?= $user['id'] ?></td> 
-                  <td><?= $user['nama'] ?></td> 
-                  <td><?= $user['npm'] ?></td> 
-                  <td><?= $user['nama_kelas'] ?></td> 
-                  <td>
-                     <a href="{{ route('users.show', $user->id) }}" class="btn btn-warning mb-3">Detail</a>
-                  </td>
+                    <th>ID</th>
+                    <th>Nama</th>
+                    <th>NPM</th>
+                    <th>Kelas</th>
+                    <th>Foto</th>
+                    <th>Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+            @foreach ($users as $user)
+                <tr>
+                    <td>{{ $user['id'] }}</td>
+                    <td>{{ $user['nama'] }}</td>
+                    <td>{{ $user['npm'] }}</td>
+                    <td>{{ $user['nama_kelas'] }}</td>
+                    <td>
+                        <img src="{{ asset('' . $user->foto) }}" alt="Foto User" class="img-thumbnail" width="100">
+                    </td>
+                    <td>
+                        <!-- View -->
+                        <a href="{{ route('users.show', $user['id']) }}" class="btn btn-info btn-sm">View</a>
+                        <!-- Edit -->
+                        <a href="{{ route('user.edit', $user['id']) }}" class="btn btn-warning btn-sm">Edit</a>
+                        <!-- Delete -->
+                        <form action="{{ route('user.destroy', $user['id']) }}" method="POST" class="d-inline">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger btn-sm"
+                                onclick="return confirm('Apakah Anda yakin ingin menghapus user ini?')">Delete</button>
+                        </form>
+                    </td>
                 </tr>
             @endforeach
-        </tbody>
-    </table>
+            </tbody>
+        </table>
+    </div>
 </div>
 @endsection
---
+++ resources/views/show_user.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="profile-container">
+    @if ($user->foto) <!-- Cek jika ada foto -->
+            <img src="{{ asset($user->foto) }}" alt="Profile Picture"> <!-- Tampilkan foto pengguna -->
+        @else
+            <img src="https://assets-a1.kompasiana.com/items/album/2021/03/24/blank-profile-picture-973460-1280-605aadc08ede4874e1153a12.png?t=o&v=780" alt="Default Profile Picture"> <!-- Gambar default -->
+        @endif    
+    <table>
+    <tr> 
+            <td>{{ $user->nama }}</td> 
+        </tr> 
+        <tr>   
+            <td>{{ $kelas->nama_kelas ?? 'Kelas tidak ditemukan' }}</td> 
+        </tr> 
+        <tr> 
+            <td>{{ $user->npm }}</td> 
+        </tr>  
+        </table>
+    </div>
+@endsection
